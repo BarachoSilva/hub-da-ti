4dde6ef8f8ec84c8065bf0865028ba93898295dd
Alex Baracho <alex_rodrigues_11@hotmail.com>
2023-12-31 07:35:20 -0300

    atualizações

--- cursos/script.js
+++ cursos/script.js
@@ -1,8 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuBtn = document.getElementById("menuBtn");
-  const nav = document.querySelector(".nav");
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.getElementById('menuBtn');
+    const nav = document.querySelector('.nav');
 
-  menuBtn.addEventListener("click", function () {
-    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-  });
+    menuBtn.addEventListener('click', function () {
+        nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    // Feche o menu ao clicar em um item de navegação
+    const navLinks = document.querySelectorAll('.ul a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            nav.style.display = 'none';
+        });
+    });
+
+    // Feche o menu ao clicar fora dele
+    document.addEventListener('click', function (event) {
+        if (!event.target.closest('.header')) {
+            nav.style.display = 'none';
+        }
+    });
 });
